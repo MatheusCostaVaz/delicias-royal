fa988e59171be4cfd5a8cc5fde445533049935c1
Matheus Vaz <matheuscostav@outlook.com>
2023-11-11 15:04:47 -0300

    Criação pasta scripts e script do menu

---
+++ scripts/menu.js
@@ -0,0 +1,25 @@
+function scrollToAprentacao() {
+    var section = document.querySelector('.app__section-banner-container');
+
+    window.scrollTo({
+        top: section.offsetTop,
+        behavior: 'smooth'
+    });
+}
+function scrollToProdutos() {
+    var section = document.querySelector('.titulo-products');
+
+    window.scrollTo({
+        top: section.offsetTop,
+        behavior: 'smooth'
+    });
+}
+
+function scrollToCarrinho() {
+    var section = document.querySelector('.titulo-products');
+
+    window.scrollTo({
+        top: section.offsetTop,
+        behavior: 'smooth'
+    });
+}
