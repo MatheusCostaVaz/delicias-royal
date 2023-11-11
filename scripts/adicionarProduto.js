a6fb22ceecc60912e734da85047fc0bee675e513
Matheus Vaz <matheuscostav@outlook.com>
2023-11-11 16:59:10 -0300

    Adicionado o script do adcionarProdutos

--- scripts/adicionarProduto.js
+++ scripts/adicionarProduto.js
@@ -1,3 +1,25 @@
-function adicionarProduto(add){
-    
-}
+const cards = document.querySelectorAll('.app__card-products');
+
+// Itera sobre cada card
+cards.forEach(card => {
+  // Para cada card, seleciona os elementos específicos dentro do card
+  const diminuirButton = card.querySelector('.app__section-products__button--diminuir');
+  const aumentarButton = card.querySelector('.app__section-products__button--aumentar');
+  const box = card.querySelector('.box');
+
+  // Adiciona evento de clique para o botão de diminuir
+  diminuirButton.addEventListener('click', () => {
+    let quantidade = parseInt(box.textContent);
+    if (quantidade > 0) {
+      quantidade--;
+      box.textContent = quantidade;
+    }
+  });
+
+  // Adiciona evento de clique para o botão de aumentar
+  aumentarButton.addEventListener('click', () => {
+    let quantidade = parseInt(box.textContent);
+    quantidade++;
+    box.textContent = quantidade;
+  });
+});
